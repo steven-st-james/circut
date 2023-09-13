dd8fb44f864f51473593ba0a245d67b52d4d7a1e
Steven Brown <stevenbrown@stevens-mini.lan>
2023-09-13 07:30:19 -0600

    started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  BehaviorSubject, combineLatest, from, mergeAll, Observable, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'circut-app';
+  num: number =  0;
+  sentense: string = "";
+  first: number = 0
+  second: number = 0;
+  firstBit = new Subject<number>()
+  secondBit = new Subject<number>()
+
+  handleFirst() {
+    this.firstBit.next(this.first)
+    // this.firstBit.complete()
+    
+  }
+
+  handleSecond() {
+    this.secondBit.next(this.second);
+    // this.secondBit.complete();
+  }
+
+  
+
+  $mergeAll = combineLatest([this.firstBit, this.secondBit], (f, s) => { return f + s})
+  increment() {
+     this.num = this.num + 1;
+  }
+
+  decrement() {
+     this.num =  this.num -1;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UppercasePipe } from './pipes/uppercase.pipe';
+import { ProjectionComponent } from './components/projection/projection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UppercasePipe,
+    ProjectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
